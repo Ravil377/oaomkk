5d728ca23ca47413c50092f0b818fed4dce0d54f
Ravil Shamilov <75186385+Ravil377@users.noreply.github.com>
2022-05-09 23:52:23 +0500

    paralax

--- src/components/components.js
+++ src/components/components.js
@@ -1,5 +1,8 @@
+// import simpleParallax from 'simple-parallax-js';
+
 // Form
 // import './form/form'
+import './who-goes/who-goes'
 
 // Hamburger
 //import './hamburger/hamburger'
---
+++ src/components/who-goes/who-goes.js
@@ -0,0 +1,6 @@
+const image = document.getElementsByClassName('bgparalax');
+new simpleParallax(image, {
+    orientation: 'down',
+    scale: 1.8,
+    overflow: true,
+});
